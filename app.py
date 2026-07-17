9eef597c4c42d6c8a6056c1709ad5b317328f566
Semia Ben Amara <benamara.semia@gmail.com>
2026-07-17 23:22:29 +0200

    update: update the adress of unified_drone_network.csv

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.set_page_config(page_title=app_full_name, layout="wide", page_icon="🚁")
 # --- 1. DATA LOADING FUNCTIONS ---
 @st.cache_data
 def load_data():
-    hubs = pd.read_csv('unified_drone_network.csv')
+    hubs = pd.read_csv('../data/unified_drone_network.csv')
     facilities = pd.read_csv('df_health_diagnostic.csv')
     return hubs, facilities
 
